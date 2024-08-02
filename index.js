dafdc6fd43ee9a5a18038c079b13548508a57b95
whymprn <wahyuimpron86@gmail.com>
2024-08-02 10:06:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ const menu = require("./lib/menu.js")
 
 const apivhtear = 'Apikey vhtear';
 const apibarbar = 'Apikey mhankbarbar';
-const BotName = 'VVIPINDO BOT'; 
-const instagram = 'agus_alnsyh71- kaelMk_'; 
-const aktif = 'Kapan bot aktif';
+const BotName = 'WHYMPRN.BOT'; 
+const instagram = 'whymprn_'; 
+const aktif = 'Terserah yang ngeaktifin';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
+            + 'FN:Wahyu Impron\n' // Nama kamu
+            + 'ORG:WHYMPRN.BOT;\n' // Nama bot
+            + 'TEL;type=CELL;type=VOICE;waid=6285782134660:+62 857-8213-4660\n' //Nomor whatsapp kamu
             + 'END:VCARD'
 const
 {
